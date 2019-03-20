31bf251974649f830fcc87d8cfc285ccdb6872df
Evgeny Mikheev <egen@molnet.ru>
2019-03-20 10:51:41 +0300

    Updated min JDK to 1.8 JSONArray and JSONValue generify and small improvements All static toJSONString and writeJSONString methods moved to new class JSONWriter

--- src/main/java/org/json/simple/JSONObject.java
+++ src/main/java/org/json/simple/JSONObject.java
@@ -1,132 +1,99 @@
-/*
- * $Id: JSONObject.java,v 1.1 2006/04/15 14:10:48 platform Exp $
- * Created on 2006-4-10
- */
 package org.json.simple;
 
+import org.json.simple.writer.JSONWriter;
+
 import java.io.IOException;
-import java.io.StringWriter;
+import java.io.Serializable;
 import java.io.Writer;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 /**
  * A JSON object. Key value pairs are unordered. JSONObject supports java.util.Map interface.
- * 
- * @author FangYidong<fangyidong@yahoo.com.cn>
+ *
+ * @author FangYidong<fangyidong @ yahoo.com.cn>
  */
-public class JSONObject extends HashMap implements Map, JSONAware, JSONStreamAware{
-	
+public class JSONObject<K, V> extends HashMap<K, V> implements Map<K, V>, JSONAware, JSONStreamAware, Serializable {
+
 	private static final long serialVersionUID = -503443796854799292L;
-	
-	
+
+	/**
+	 * Constructs an empty JSONObject.
+	 */
 	public JSONObject() {
 		super();
 	}
 
 	/**
+	 * Constructs an empty JSONObject with the specified initial capacity and the default load factor (0.75).
+	 *
+	 * @param initialCapacity the initial capacity
+	 */
+	public JSONObject(int initialCapacity) {
+		super(initialCapacity);
+	}
+
+	/**
+	 * Constructs an empty JSONObject with the specified initial capacity and load factor.
+	 *
+	 * @param initialCapacity the initial capacity
+	 * @param loadFactor      the load factor
+	 */
+	public JSONObject(int initialCapacity, float loadFactor) {
+		super(initialCapacity, loadFactor);
+	}
+
+	/**
 	 * Allows creation of a JSONObject from a Map. After that, both the
 	 * generated JSONObject and the Map can be modified independently.
-	 * 
+	 *
 	 * @param map
 	 */
-	public JSONObject(Map map) {
+	public JSONObject(Map<K, V> map) {
 		super(map);
 	}
 
 
-    /**
-     * Encode a map into JSON text and write it to out.
-     * If this map is also a JSONAware or JSONStreamAware, JSONAware or JSONStreamAware specific behaviours will be ignored at this top level.
-     * 
-     * @see org.json.simple.JSONValue#writeJSONString(Object, Writer)
-     * 
-     * @param map
-     * @param out
-     */
-	public static void writeJSONString(Map map, Writer out) throws IOException {
-		if(map == null){
-			out.write("null");
-			return;
+	/**
+	 * Return value typed as T. If value is null or cast exception return default value
+	 *
+	 * @param key          key
+	 * @param defaultValue default value
+	 * @param <T>          result type
+	 * @return if exists value otherwise defaultValue
+	 */
+	public <T> T get(Object key, T defaultValue) {
+		try {
+			return (T) super.getOrDefault(key, (V) defaultValue);
+		} catch (Exception e) {
+			return defaultValue;
 		}
-		
-		boolean first = true;
-		Iterator iter=map.entrySet().iterator();
-		
-        out.write('{');
-		while(iter.hasNext()){
-            if(first)
-                first = false;
-            else
-                out.write(',');
-			Map.Entry entry=(Map.Entry)iter.next();
-            out.write('\"');
-            out.write(escape(String.valueOf(entry.getKey())));
-            out.write('\"');
-            out.write(':');
-			JSONValue.writeJSONString(entry.getValue(), out);
-		}
-		out.write('}');
 	}
 
-	public void writeJSONString(Writer out) throws IOException{
-		writeJSONString(this, out);
+	public void writeJSONString(Writer out) throws IOException {
+		JSONWriter.writeJSONString(this, out);
 	}
-	
-	/**
-	 * Convert a map to JSON text. The result is a JSON object. 
-	 * If this map is also a JSONAware, JSONAware specific behaviours will be omitted at this top level.
-	 * 
-	 * @see org.json.simple.JSONValue#toJSONString(Object)
-	 * 
-	 * @param map
-	 * @return JSON text, or "null" if map is null.
-	 */
-	public static String toJSONString(Map map){
-		final StringWriter writer = new StringWriter();
-		
-		try {
-			writeJSONString(map, writer);
-			return writer.toString();
-		} catch (IOException e) {
-			// This should never happen with a StringWriter
-			throw new RuntimeException(e);
-		}
+
+	public String toJSONString() {
+		return JSONWriter.toJSONString(this);
 	}
-	
-	public String toJSONString(){
-		return toJSONString(this);
-	}
-	
-	public String toString(){
+
+	public String toString() {
 		return toJSONString();
 	}
 
-	public static String toString(String key,Object value){
-        StringBuffer sb = new StringBuffer();
-        sb.append('\"');
-        if(key == null)
-            sb.append("null");
-        else
-            JSONValue.escape(key, sb);
+	public static String toString(String key, Object value) {
+		StringBuffer sb = new StringBuffer();
+		sb.append('\"');
+		if (key == null)
+			sb.append("null");
+		else
+			JSONValue.escape(key, sb);
 		sb.append('\"').append(':');
-		
+
 		sb.append(JSONValue.toJSONString(value));
-		
+
 		return sb.toString();
 	}
-	
-	/**
-	 * Escape quotes, \, /, \r, \n, \b, \f, \t and other control characters (U+0000 through U+001F).
-	 * It's the same as JSONValue.escape() only for compatibility here.
-	 * 
-	 * @see org.json.simple.JSONValue#escape(String)
-	 * 
-	 * @param s
-	 * @return
-	 */
-	public static String escape(String s){
-		return JSONValue.escape(s);
-	}
+
 }
---
+++ src/main/java/org/json/simple/writer/JSONWriter.java
@@ -0,0 +1,460 @@
+package org.json.simple.writer;
+
+import org.json.simple.JSONValue;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * Convert array to string
+ *
+ * @author Evgeny Mikheev
+ */
+public class JSONWriter {
+	/**
+	 * Convert a list to JSON text. The result is a JSON array.
+	 * If this list is also a JSONAware, JSONAware specific behaviours will be omitted at this top level.
+	 *
+	 * @param collection
+	 * @return JSON text, or "null" if list is null.
+	 * @see org.json.simple.JSONValue#toJSONString(Object)
+	 */
+	public static String toJSONString(Collection collection) {
+		final StringWriter writer = new StringWriter();
+
+		try {
+			writeJSONString(collection, writer);
+			return writer.toString();
+		} catch (IOException e) {
+			// This should never happen for a StringWriter
+			throw new RuntimeException(e);
+		}
+	}
+
+	/**
+	 * Convert a map to JSON text. The result is a JSON object.
+	 * If this map is also a JSONAware, JSONAware specific behaviours will be omitted at this top level.
+	 *
+	 * @param map
+	 * @return JSON text, or "null" if map is null.
+	 * @see org.json.simple.JSONValue#toJSONString(Object)
+	 */
+	public static String toJSONString(Map map) {
+		final StringWriter writer = new StringWriter();
+
+		try {
+			writeJSONString(map, writer);
+			return writer.toString();
+		} catch (IOException e) {
+			// This should never happen with a StringWriter
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static String toJSONString(Object[] array) {
+		final StringWriter writer = new StringWriter();
+
+		try {
+			writeJSONString(array, writer);
+			return writer.toString();
+		} catch (IOException e) {
+			// This should never happen for a StringWriter
+			throw new RuntimeException(e);
+		}
+	}
+
+	/**
+	 * Convert array to JSON array string
+	 *
+	 * @param array source array
+	 * @return JSON array string
+	 */
+	public static String toJSONString(byte[] array) {
+		final StringWriter writer = new StringWriter();
+		try {
+			writeJSONString(array, writer);
+		} catch (IOException e) {
+			e.printStackTrace(System.err);
+		}
+		return writer.toString();
+	}
+
+	/**
+	 * Convert array to JSON array string
+	 *
+	 * @param array source array
+	 * @return JSON array string
+	 */
+	public static String toJSONString(short[] array) {
+		final StringWriter writer = new StringWriter();
+		try {
+			writeJSONString(array, writer);
+		} catch (IOException e) {
+			e.printStackTrace(System.err);
+		}
+		return writer.toString();
+	}
+
+	/**
+	 * Convert array to JSON array string
+	 *
+	 * @param array source array
+	 * @return JSON array string
+	 */
+	public static String toJSONString(int[] array) {
+		final StringWriter writer = new StringWriter();
+		try {
+			writeJSONString(array, writer);
+		} catch (IOException e) {
+			e.printStackTrace(System.err);
+		}
+		return writer.toString();
+	}
+
+	/**
+	 * Convert array to JSON array string
+	 *
+	 * @param array source array
+	 * @return JSON array string
+	 */
+	public static String toJSONString(long[] array) {
+		final StringWriter writer = new StringWriter();
+		try {
+			writeJSONString(array, writer);
+		} catch (IOException e) {
+			e.printStackTrace(System.err);
+		}
+		return writer.toString();
+	}
+
+	/**
+	 * Convert array to JSON array string
+	 *
+	 * @param array source array
+	 * @return JSON array string
+	 */
+	public static String toJSONString(float[] array) {
+		final StringWriter writer = new StringWriter();
+		try {
+			writeJSONString(array, writer);
+		} catch (IOException e) {
+			e.printStackTrace(System.err);
+		}
+		return writer.toString();
+	}
+
+	/**
+	 * Convert array to JSON array string
+	 *
+	 * @param array source array
+	 * @return JSON array string
+	 */
+	public static String toJSONString(double[] array) {
+		final StringWriter writer = new StringWriter();
+		try {
+			writeJSONString(array, writer);
+		} catch (IOException e) {
+			e.printStackTrace(System.err);
+		}
+		return writer.toString();
+	}
+
+	/**
+	 * Convert array to JSON array string
+	 *
+	 * @param array source array
+	 * @return JSON array string
+	 */
+	public static String toJSONString(boolean[] array) {
+		final StringWriter writer = new StringWriter();
+		try {
+			writeJSONString(array, writer);
+		} catch (IOException e) {
+			e.printStackTrace(System.err);
+		}
+		return writer.toString();
+	}
+
+	/**
+	 * Convert array to JSON array string
+	 *
+	 * @param array source array
+	 * @return JSON array string
+	 */
+	public static String toJSONString(char[] array) {
+		final StringWriter writer = new StringWriter();
+		try {
+			writeJSONString(array, writer);
+		} catch (IOException e) {
+			e.printStackTrace(System.err);
+		}
+		return writer.toString();
+	}
+
+	public static void writeJSONString(Object[] array, Writer out) throws IOException {
+		if (array == null) {
+			out.write("null");
+		} else if (array.length == 0) {
+			out.write("[]");
+		} else {
+			out.write("[");
+			JSONValue.writeJSONString(array[0], out);
+
+			for (int i = 1; i < array.length; i++) {
+				out.write(",");
+				JSONValue.writeJSONString(array[i], out);
+			}
+
+			out.write("]");
+		}
+	}
+
+	/**
+	 * Encode a list into JSON text and write it to out.
+	 * If this list is also a JSONStreamAware or a JSONAware, JSONStreamAware and JSONAware specific behaviours will be ignored at this top level.
+	 *
+	 * @param collection
+	 * @param out
+	 * @see org.json.simple.JSONValue#writeJSONString(Object, Writer)
+	 */
+	public static <T> void writeJSONString(Collection<T> collection, Writer out) throws IOException {
+		if (collection == null) {
+			out.write("null");
+			return;
+		}
+
+		boolean first = true;
+		Iterator iter = collection.iterator();
+
+		out.write('[');
+		while (iter.hasNext()) {
+			if (first)
+				first = false;
+			else
+				out.write(',');
+
+			Object value = iter.next();
+			if (value == null) {
+				out.write("null");
+				continue;
+			}
+
+			JSONValue.writeJSONString(value, out);
+		}
+		out.write(']');
+	}
+
+
+	/**
+	 * Encode a map into JSON text and write it to out.
+	 * If this map is also a JSONAware or JSONStreamAware, JSONAware or JSONStreamAware specific behaviours will be ignored at this top level.
+	 *
+	 * @param map
+	 * @param out
+	 * @see org.json.simple.JSONValue#writeJSONString(Object, Writer)
+	 */
+	public static <K, V> void writeJSONString(Map<K, V> map, Writer out) throws IOException {
+		if (map == null) {
+			out.write("null");
+			return;
+		}
+
+		boolean first = true;
+		Iterator iter = map.entrySet().iterator();
+
+		out.write('{');
+		while (iter.hasNext()) {
+			if (first)
+				first = false;
+			else
+				out.write(',');
+			Map.Entry entry = (Map.Entry) iter.next();
+			out.write('\"');
+			out.write(JSONValue.escape(String.valueOf(entry.getKey())));
+			out.write('\"');
+			out.write(':');
+			JSONValue.writeJSONString(entry.getValue(), out);
+		}
+		out.write('}');
+	}
+
+	/**
+	 * Convert array to JSON array string and write it to out
+	 *
+	 * @param array source array
+	 * @param out   output
+	 * @throws IOException write errors
+	 */
+	public static void writeJSONString(byte[] array, Writer out) throws IOException {
+		if (array == null) {
+			out.write("null");
+		} else if (array.length == 0) {
+			out.write("[]");
+		} else {
+			out.write("[");
+			out.write(String.valueOf(array[0]));
+			for (int i = 1; i < array.length; i++) {
+				out.write("," + array[i]);
+			}
+			out.write("]");
+		}
+	}
+
+	/**
+	 * Convert array to JSON array string and write it to out
+	 *
+	 * @param array source array
+	 * @param out   output
+	 * @throws IOException write errors
+	 */
+	public static void writeJSONString(short[] array, Writer out) throws IOException {
+		if (array == null) {
+			out.write("null");
+		} else if (array.length == 0) {
+			out.write("[]");
+		} else {
+			out.write("[");
+			out.write(String.valueOf(array[0]));
+			for (int i = 1; i < array.length; i++) {
+				out.write("," + array[i]);
+			}
+			out.write("]");
+		}
+	}
+
+	/**
+	 * Convert array to JSON array string and write it to out
+	 *
+	 * @param array source array
+	 * @param out   output
+	 * @throws IOException write errors
+	 */
+	public static void writeJSONString(int[] array, Writer out) throws IOException {
+		if (array == null) {
+			out.write("null");
+		} else if (array.length == 0) {
+			out.write("[]");
+		} else {
+			out.write("[");
+			out.write(String.valueOf(array[0]));
+			for (int i = 1; i < array.length; i++) {
+				out.write("," + array[i]);
+			}
+			out.write("]");
+		}
+	}
+
+	/**
+	 * Convert array to JSON array string and write it to out
+	 *
+	 * @param array source array
+	 * @param out   output
+	 * @throws IOException write errors
+	 */
+	public static void writeJSONString(long[] array, Writer out) throws IOException {
+		if (array == null) {
+			out.write("null");
+		} else if (array.length == 0) {
+			out.write("[]");
+		} else {
+			out.write("[");
+			out.write(String.valueOf(array[0]));
+			for (int i = 1; i < array.length; i++) {
+				out.write("," + array[i]);
+			}
+			out.write("]");
+		}
+	}
+
+	/**
+	 * Convert array to JSON array string and write it to out
+	 *
+	 * @param array source array
+	 * @param out   output
+	 * @throws IOException write errors
+	 */
+	public static void writeJSONString(float[] array, Writer out) throws IOException {
+		if (array == null) {
+			out.write("null");
+		} else if (array.length == 0) {
+			out.write("[]");
+		} else {
+			out.write("[");
+			out.write(String.valueOf(array[0]));
+			for (int i = 1; i < array.length; i++) {
+				out.write("," + array[i]);
+			}
+			out.write("]");
+		}
+	}
+
+	/**
+	 * Convert array to JSON array string and write it to out
+	 *
+	 * @param array source array
+	 * @param out   output
+	 * @throws IOException write errors
+	 */
+	public static void writeJSONString(double[] array, Writer out) throws IOException {
+		if (array == null) {
+			out.write("null");
+		} else if (array.length == 0) {
+			out.write("[]");
+		} else {
+			out.write("[");
+			out.write(String.valueOf(array[0]));
+			for (int i = 1; i < array.length; i++) {
+				out.write("," + array[i]);
+			}
+			out.write("]");
+		}
+	}
+
+	/**
+	 * Convert array to JSON array string and write it to out
+	 *
+	 * @param array source array
+	 * @param out   output
+	 * @throws IOException write errors
+	 */
+	public static void writeJSONString(boolean[] array, Writer out) throws IOException {
+		if (array == null) {
+			out.write("null");
+		} else if (array.length == 0) {
+			out.write("[]");
+		} else {
+			out.write("[");
+			out.write(String.valueOf(array[0]));
+			for (int i = 1; i < array.length; i++) {
+				out.write("," + array[i]);
+			}
+			out.write("]");
+		}
+	}
+
+	/**
+	 * Convert array to JSON array string and write it to out
+	 *
+	 * @param array source array
+	 * @param out   output
+	 * @throws IOException write errors
+	 */
+	public static void writeJSONString(char[] array, Writer out) throws IOException {
+		if (array == null) {
+			out.write("null");
+		} else if (array.length == 0) {
+			out.write("[]");
+		} else {
+			out.write("[");
+			out.write("\"" + array[0] + "\"");
+			for (int i = 1; i < array.length; i++) {
+				out.write(",\"" + array[i] + "\"");
+			}
+			out.write("]");
+		}
+	}
+}
